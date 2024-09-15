3598a04afe5ef32a3979e36ac17c2a5a683722e5
Prabodashan <prabodashan14@gmail.com>
2024-09-15 13:52:01 +0530

    Build account listing and messaged screens

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import AccountScreen from "./app/screens/AccountScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <ListingsScreen />;
 }
---
+++ app/screens/AccountScreen.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { StyleSheet, View, FlatList, Text, Image } from "react-native";
+
+import Screen from "../components/Screen";
+import ListItem from "../components/ListItem";
+import ListItemSeparatorComponent from "../components/ListItemSeparator";
+import colors from "../config/colors";
+import Icon from "../components/Icon";
+import AppText from "../components/AppText";
+
+const menuItems = [
+  {
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
+    },
+  },
+  {
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+  },
+];
+
+function AccountScreen(props) {
+  return (
+    <Screen style={styles.screen}>
+      <View style={styles.container}>
+        <View style={{ flexDirection: "row" }}>
+          <Image style={styles.image} source={require("../assets/icon.png")} />
+          <View style={styles.detailsContainer}>
+            <AppText style={styles.title}>User Name</AppText>
+            <AppText style={styles.subTitle}>username@gmail.com</AppText>
+          </View>
+        </View>
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItem) => menuItem.title}
+          ItemSeparatorComponent={ListItemSeparatorComponent}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+            />
+          )}
+        />
+      </View>
+      <View style={styles.container}>
+        <View
+          style={[
+            styles.detailsContainer,
+            {
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            },
+          ]}
+        >
+          <Icon name="logout" backgroundColor="#ffe66d" />
+          <AppText style={[styles.title, { marginLeft: 10 }]}>Log Out</AppText>
+        </View>
+      </View>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: colors.light,
+  },
+  container: {
+    marginVertical: 20,
+    backgroundColor: colors.white,
+    padding: 15,
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+  },
+  subTitle: {
+    color: colors.medium,
+  },
+  title: {
+    fontWeight: "500",
+  },
+});
+
+export default AccountScreen;
